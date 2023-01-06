f1e3500d63ae26ef326eedb38b7a53028ebba336
jwest <jwest@csp.edu>
2023-01-06 11:29:45 -0600

    Completed questions 1-4

---
+++ Program1/src/Main.java
@@ -0,0 +1,47 @@
+/**
+ * Author: James West
+ * Date: 01.06.2023
+ * Class: Spring 100 CSC322 Object-Oriented Programming
+ * Assignment: Program1 - Pet Database Version 2
+ * Description: Association, Composition and Aggregation Relationships in Java
+ */
+
+/**
+ * Question Answers:
+ *
+ * 1. Relationships between classes are an important part of Object-Oriented Programming because they allow for the
+ * modeling of complex systems which contain many interconnected components at varying levels of abstraction and
+ * specificity within an application.
+ *
+ * 2. An Association is a relationship between classes which involves an activity between the classes.
+ *
+ * 3. List the four types of Associations between two classes.
+ * In reading the text, it was hard to find 4 clear types of Association. There was Aggregation, Composition, and
+ * Inheritance was mentioned. According to https://www.javatpoint.com/uml-relationship, the 4 types are
+ * bidirectional, unidirectional, aggregation (composition aggregation), and reflexive. I researched a few other
+ * sites, but these seemed to be the clearest to me given the prompt.
+ *
+
+
+ * 4. Illustrate each of these relationships by writing a sentence for each one between an Employee class and a
+ * Company class
+ *
+ * a) unidirectional: A Company hires a Employee.
+ * b) bidirectional: A Company hires an Employee and an Employee works for a Company(s).
+ * c) aggregation: A Company has an Employee.
+ * d) reflexive: A Company has an Employee (e.g., the manager) who hires Employees.
+ *
+ * 5. These Associations mean that objects from one class can communicate with objects of another class, enabling
+ * them to access data. Describe how a HAS-a relationship is created in OOP.
+ *
+ *
+ *
+ * 6. The two forms of Association are Aggregation and Composition. Describe both, highlighting the differences.
+ *
+**/
+
+// TODO - Write two classes and a main class to demonstrate how an Aggregation relationship is established and used.
+
+// TODO - Write two classes and a main class to demonstrate how a Composition relationship is established and used.
+public class Main {
+}
